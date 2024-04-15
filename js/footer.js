ac5c51c1948b2b84acdd9a0fe0490f41bb0feeeb
Daniel Brenes <danielbrenesfz@gmail.com>
2024-04-15 14:18:59 +0100

    first commit web design

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+// footer.js
+document.write('<footer>');
+document.write('  <p>Copyright 2024, Developed by Daniel - Regina - Juan. All Rights Reserved</p>');
+document.write('</footer>');
---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+// navbar.js
+document.write('<nav class="navbar">');
+document.write(' <div> <a href="../index.html"> <img class="logo" src="../img/logo.png" alt="logo"> </a> </div> ');
+document.write(' <ul class="nav-list" id="navlist"> <li class="list-item"> <a href="../index.html"> Home </a> <li class="list-item"> <a href="gallery.html"> Gallery </a> </li> <li class="list-item"> <a href="about.html"> About </a> </li> <li class="list-item"> <a href="contact.html"> Contact </a> </li> </li> <li class="list-item"> <a href="partnership.html"> Partnership </a> </li> <li class="list-item"> <a href="reviews.html"> Reviews </a> </li> </ul> ');
+document.write(' <div class="menu" id="toggleButton"><div class="menu-burger"></div><div class="menu-burger"></div><div class="menu-burger"></div></div> ');
+document.write('</nav>');
+
+var toggleButton = document.getElementById('toggleButton');
+var navList = document.getElementById('navlist');
+
+toggleButton.addEventListener('click', () =>{
+    navList.classList.toggle('active');
+})
