247d951c1baf6b17d7f917ab0c1a95ad6f8e3a6b
Daniel Brenes <danielbrenesfz@gmail.com>
2024-04-17 16:02:04 +0100

    refactoring the code and structure

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,13 @@ document.write('<nav class="navbar">');
 document.write('<div>',
                     '<a href="../index.html"> <img class="logo" src="../images/377193.svg" alt="logo"></a>',
                 '</div>');
-document.write(' <ul class="nav-list" id="navlist"> <li class="list-item"> <a href="../index.html"> Home </a> <li class="list-item"> <a href="gallery.html"> Gallery </a> </li> <li class="list-item"> <a href="about.html"> About </a> </li> <li class="list-item"> <a href="contact.html"> Contact </a> </li> </li> <li class="list-item"> <a href="collaborations.html"> Collaborations </a> </li> <li class="list-item"> <a href="reviews.html"> Reviews </a> </li> <li class="list-item"> <a href="game.html"> Game </a> </li> </ul> ');
+document.write(' <ul class="nav-list" id="navlist"> ',
+                    '<li class="list-item"> <a href="../index.html"> Home </a> </li>',
+                    '<li class="list-item"> <a href="gallery.html"> Gallery </a> </li>',
+                    '<li class="list-item"> <a href="collaborations.html"> Collaborations </a> </li>',
+                    '<li class="list-item"> <a href="reviews.html"> Reviews </a> </li>',
+                    '<li class="list-item"> <a href="about.html"> About </a> </li>',
+                    '<li class="list-item"> <a href="contact.html"> Contact </a> </li>');
 document.write(' <div class="menu" id="toggleButton"><div class="menu-burger"></div><div class="menu-burger"></div><div class="menu-burger"></div></div> ');
 document.write('</nav>');
 
