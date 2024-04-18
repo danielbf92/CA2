5edf54204f798a59aa168a1447c01292682a365d
Daniel Brenes <danielbrenesfz@gmail.com>
2024-04-18 11:59:34 +0100

    adding show info JS

---
+++ js/showInfo.js
@@ -0,0 +1,8 @@
+function showInfo() {
+    var text = document.getElementById("textField");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+}
