aec729ae54ab962080ad22daf978f904f53599b6
Daniel Brenes <danielbrenesfz@gmail.com>
2024-04-20 00:52:05 +0100

    Responsive Navbar Finished

--- js/navbar.js
+++ js/navbar.js
@@ -10,7 +10,7 @@ document.write(' <ul class="nav-list" id="navlist"> ',
 					 '<li class="list-item"> <a href="services.html"> Services </a> </li>',
                     '<li class="list-item"> <a href="reviews.html"> Reviews </a> </li>',
                     '<li class="list-item"> <a href="about.html"> About </a> </li>',
-                    '<li class="list-item"> <a href="contact.html"> Contact </a> </li>');
+                    '<li class="list-item"> <a href="contact.html"> Contact </a> </li> </ul>');
 document.write(' <div class="menu" id="toggleButton"><div class="menu-burger"></div><div class="menu-burger"></div><div class="menu-burger"></div></div> ');
 document.write('</nav>');
 
